3634a637d2125d7fa78d2c747cfe135c1ffc203a
AleXAnastasov77 <aleks13361@gmail.com>
2025-11-21 10:47:51 +0100

    refactor db to mysql

--- infra/cloud/db.tf
+++ infra/cloud/db.tf
@@ -7,24 +7,31 @@ resource "aws_db_subnet_group" "sqldb_sg" {
   }
 }
 
-resource "aws_db_instance" "sqldb_cs3" {
+resource "aws_db_instance" "mysqldb_cs3" {
   allocated_storage         = 20
   max_allocated_storage     = 25
-  db_subnet_group_name      = aws_db_subnet_group.sqldb_sg.name
-  license_model             = "license-included"
-  engine                    = "sqlserver-ee"
-  engine_version            = "15.00"
+
+  engine                    = "mysql"
+  engine_version            = "8.0" 
   instance_class            = "db.t3.micro"
+
   username                  = var.DB_USERNAME
   password                  = var.DB_PASSWORD
-  skip_final_snapshot       = false
-  final_snapshot_identifier = "HRapp-MSSQL-final-${formatdate("YYYYMMDDhhmmss", timestamp())}"
-  storage_type              = "gp2"
+
+  db_subnet_group_name      = aws_db_subnet_group.sqldb_sg.name
   vpc_security_group_ids    = [aws_security_group.db_sg.id]
+
+  
   storage_encrypted         = true
+
+  skip_final_snapshot       = false
+  final_snapshot_identifier = "HRapp-MySQL-final-${formatdate("YYYYMMDDhhmmss", timestamp())}"
+
+  backup_retention_period   = 7
+
   lifecycle {
     ignore_changes = [
-      password, # Don’t try to reset the password every run
+      password,
       backup_retention_period,
       maintenance_window,
       final_snapshot_identifier
